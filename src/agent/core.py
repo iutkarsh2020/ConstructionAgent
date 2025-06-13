d83a5e9512cf49a67b6e5b98866c12a8569eee3d
iutkarsh2020 <utkarshsharma@Utkarshs-MacBook-Air.local>
2025-06-13 02:01:19 -0700

    Adding Structure

--- src/agent/core.py
+++ src/agent/core.py
@@ -2,40 +2,35 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 import asyncio
-from langchain_mcp_adapters.client import MultiServerMCPClient
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage, ToolCall 
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import tools_condition, ToolNode
 from langgraph.checkpoint.memory import MemorySaver
 from pathlib import Path
-
+from agent.mcp_layer import MCPLayer
 from agent.state import MessagesState
-from agent.mcp_config import MCP_CLIENT_CONFIG
+from agent.mcp_config import MCP_CLIENT_CONFIG, REQUIRED_PROMPT_NAMES
+import json
+import random
 
 load_dotenv()
-
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 GOOGLE_GENAI_MODEL = os.environ.get('GOOGLE_GENAI_MODEL')
-
 # Get the project root directory
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 
 class AgentGraph:
     def __init__(self):
-        self.client = MultiServerMCPClient(MCP_CLIENT_CONFIG)
+        self.mcp_client = MCPLayer()
         self.llm = ChatGoogleGenerativeAI(model=GOOGLE_GENAI_MODEL, temperature=0, google_api_key=GOOGLE_API_KEY)
         self.tools = None
-        self.llm_with_tools = None
-        self.sys_prompt = None
-        self.query_validation_prompt = None
+        self.prompts = None
         self.graph = None
 
     async def fetch_tools_and_prompts(self):
-        self.tools = await self.client.get_tools()
+        self.tools = await self.mcp_client.fetch_tools()
         self.llm_with_tools = self.llm.bind_tools(self.tools)
-        self.sys_prompt = await self.client.get_prompt(server_name="prompt_server", prompt_name="system_prompt")
-        self.query_validation_prompt = await self.client.get_prompt(server_name="prompt_server", prompt_name="query_validation_prompt")
-        self.clarification_prompt = await self.client.get_prompt(server_name="prompt_server", prompt_name="clarification_prompt")
+        self.prompts = await self.mcp_client.fetch_prompts(list(REQUIRED_PROMPT_NAMES.keys()))
 
     async def get_tool_descriptions(self):
         descriptions = []
@@ -57,7 +52,7 @@ class AgentGraph:
         user_query = state['messages'][-1]
         tool_descriptions = await self.get_tool_descriptions()
         validation_sys_message = SystemMessage(
-            content=self.query_validation_prompt[0].content.format(tool_descriptions=tool_descriptions)
+            content=self.prompts['query_validation_prompt'][0].content.format(tool_descriptions=tool_descriptions)
         )
         result = await self.llm_with_tools.ainvoke([validation_sys_message, user_query])
         return {'messages': [result]}
@@ -72,34 +67,38 @@ class AgentGraph:
             query = json.loads(query)
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
+        to_return = {}
         if query.get("unrelated", 'true'):
-            return {
+            to_return = {
                 "messages": [
                     AIMessage(content="Sorry, the query seems unfamiliar. I am a construction assistant and I can only help with construction related tasks.")
                 ]
             }
-        intents = query.get("intents", [])
-        ambiguous_intents = [intent for intent in intents if intent["is_ambiguous"]]
-        if ambiguous_intents:
-            clarification_prompt = SystemMessage(content=self.clarification_prompt.content)
-            human_message = HumanMessage(content=json.dumps(ambiguous_intents, indent=2))
-            clarification_response = await self.llm_with_tools.ainvoke([clarification_prompt, human_message])
-            return {
-                "messages": [clarification_response]
-            }
-        tool_calls = [
-            ToolCall(
-                id=f"call_{int(random.random() * 1e18)}",  # large 18-digit integer
-                name=intent["tool"],
-                args=intent["arguments"]
-            )
-            for intent in intents
-        ]
-        tool_messages = AIMessage(
-            content="",
-            tool_calls=tool_calls
-        )
-        return {"messages": tool_messages}
+        else: 
+            intents = query.get("intents", [])
+            ambiguous_intents = [intent for intent in intents if intent["is_ambiguous"]]
+            if ambiguous_intents:
+                clarification_prompt = SystemMessage(content=self.prompts['clarification_prompt'][0].content)
+                human_message = HumanMessage(content=json.dumps(ambiguous_intents, indent=2))
+                clarification_response = await self.llm_with_tools.ainvoke([clarification_prompt, human_message])
+                to_return = {
+                    "messages": [clarification_response]
+                }
+            else:
+                tool_calls = [
+                    ToolCall(
+                        id=f"call_{int(random.random() * 1e18)}",  # large 18-digit integer
+                        name=intent["tool"],
+                        args=intent["arguments"]
+                    )
+                    for intent in intents
+                ]
+                # tool_messages are returned as AIMessage
+                to_return = {'messages':[AIMessage(
+                    content="",
+                    tool_calls=tool_calls
+                )]}
+        return to_return
 
     async def build_graph(self):
         await self.fetch_tools_and_prompts()
@@ -114,4 +113,3 @@ class AgentGraph:
         builder.add_edge('tools', 'Agent')
         memory = MemorySaver()
         self.graph = builder.compile(checkpointer=memory)
-        return self.graph, self.tools, self.sys_prompt, self.query_validation_prompt
--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-from agent.core import create_graph
+from agent.core import AgentGraph
 import asyncio
-
+from langchain_core.messages import HumanMessage
 
 async def main():
-    
-    graph, tools, sys_prompt, query_validation_prompt = await create_graph()
-    # filtered_tools = get_tool_descriptions(tools)
-    # print(filtered_tools)
+    agent_graph = AgentGraph()
+    await agent_graph.build_graph()
+    graph = agent_graph.graph
+
+    thread_config = {'configurable': {'thread_id': '1'}}
+    result = await graph.ainvoke({'messages':[HumanMessage(content="What is the area of region A and scale of drawing V?")]}, config=thread_config)
+    print(result)
 
 if __name__ == "__main__":
     asyncio.run(main())
